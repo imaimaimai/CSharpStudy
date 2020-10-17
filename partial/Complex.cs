25c432a2aaf000149eef3de9a5108f08514a9ea2
imai@weel.co.jp <imai@weel.co.jp>
2020-10-17 22:46:31 +0900

    プロジェクト ファイルを追加します。

---
+++ partial/Complex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace partial
+{
+    public partial class Complex
+    {
+        public double re;
+        public double im;
+
+        public Complex(double re, double im)
+        {
+            this.re = re;
+            this.im = im;
+        }
+
+    }
+}
---
+++ partial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace partial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c = new Complex(10, 10);
+
+            Console.WriteLine(c.Abs());
+        }
+    }
+
+    public partial class Complex
+    {
+        public double Abs()
+        {
+            return Math.Sqrt(re * re + im * im);
+        }
+    }
+}
+
+
