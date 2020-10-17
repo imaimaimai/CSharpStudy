85798ae866f6182dc303b42d409220d9aecc5732
imai@weel.co.jp <imai@weel.co.jp>
2020-10-17 23:00:25 +0900

    Constructor added

---
+++ Constructor/Person.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Constructor
+{
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person()
+        {
+            this.name = "taro";
+            this.age = 100000;
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Person p2 = new Person("Jiro", 0);
+
+            Console.WriteLine($"{p1.name}さん、{p1.age}歳");
+            Console.WriteLine($"{p2.name}さん、{p2.age}歳");
+        }
+    }
+}
